4cf686db1661e7c2502bd35ea52504042b1e24e4
Caleb Fahlgren <calebfahlgren@protonmail.com>
2021-04-10 13:58:56 -0400

    add project model

--- api/api/apps/projects/models.py
+++ api/api/apps/projects/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    COMPLEXITY_CHOICES = [('Beginner', 'Beginner'), ('Intermediate',
+                                                     'Intermediate'), ('Advanced', 'Advanced'), ]
+
+    name = models.CharField(max_length=60)
+    start_date = models.DateField()
+    complexity = models.CharField(max_length=50, choices=COMPLEXITY_CHOICES)
+    technologies = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    project_url = models.URLField()
