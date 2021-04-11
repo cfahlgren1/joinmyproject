bcd67027d7916b4d98cf0a12ef0c64464156de8f
Caleb Fahlgren <calebfahlgren@protonmail.com>
2021-04-11 11:28:35 -0400

    add cards from projects API

--- justmyproject/src/components/ProjectCardDeck.js
+++ justmyproject/src/components/ProjectCardDeck.js
@@ -1,12 +1,40 @@
 import { CardColumns, Container } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
 import ProjectCard from "./ProjectCard";
+import axios from "axios";
 
 import "./ProjectCardDeck.css";
 
 const ProjectCardDeck = () => {
+  const [isLoaded, setLoaded] = useState(false);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(async () => {
+    const projects = await axios.get(
+      `http://142.93.118.235:8000/api/projects/`
+    );
+
+    setProjects(projects.data);
+    setLoaded(true);
+  }, []);
+
+  if (!isLoaded) {
+    return <p>Loading....</p>;
+  }
   return (
     <Container>
-      <CardColumns></CardColumns>
+      <CardColumns>
+        {projects.map((proj) => (
+          <ProjectCard
+            name={proj.name}
+            description={proj.description}
+            technologies={proj.technologies}
+            project_url={proj.project_url}
+            complexities={proj.complexities}
+            start_date={proj.start_date}
+          />
+        ))}
+      </CardColumns>
     </Container>
   );
 };
