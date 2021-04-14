c5da3ed99b2f9b71bc891dfabd976c9c2f494814
Caleb Fahlgren <calebfahlgren@protonmail.com>
2021-04-13 21:53:10 -0400

    seperate settings files

---
+++ api/api/settings/production.py
@@ -0,0 +1,12 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS += ['joinmyproject.calebfahlgren.com']
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_SSL_REDIRECT = True
